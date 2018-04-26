728f73092f99b0916a788f684051fe2bd4b41f5e
akaczmarek <adam.kaczmarek@onwelo.com>
2018-04-26 15:39:32 +0200

    Refactoring + update version.

--- src/main/java/home/samples/context/UserContextInterceptor.java
+++ src/main/java/home/samples/context/UserContextInterceptor.java
@@ -10,14 +10,15 @@ import org.springframework.http.client.ClientHttpResponse;
 import java.io.IOException;
 
 public class UserContextInterceptor implements ClientHttpRequestInterceptor {
-    @Override
+
     public ClientHttpResponse intercept(HttpRequest request, byte[] body,
 
         ClientHttpRequestExecution execution) throws IOException {
         HttpHeaders headers = request.getHeaders();
-        headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
-        headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
-        headers.add(UserContext.USER_ID, UserContextHolder.getContext().getUserId());
+
+        if(!headers.containsKey(UserContext.CORRELATION_ID)) headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
+        if(!headers.containsKey(UserContext.AUTH_TOKEN))headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
+        if(!headers.containsKey(UserContext.USER_ID))headers.add(UserContext.USER_ID, UserContextHolder.getContext().getUserId());
 
         return execution.execute(request, body);
 
